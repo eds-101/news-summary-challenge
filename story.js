d63e512fdda898d8a515a175f6d06f40faf30a77
Eds <steadyede@live.co.uk>
2021-03-20 13:48:32 +0000

    api clean method

--- story.js
+++ story.js
@@ -2,11 +2,18 @@ class Story {
   constructor (title) {
     this.title = title
     this.apiUrl = ""
-    this.id = ""
+    this.cleanApiUrl = ""
     this.webTitle = ""
-    this.webUrl = ""
-    this.apiUrl = ""
     this.img = ""
     this.text = ""
   }
+
+  cleanUrl() {
+    this.cleanApiUrl = this.apiUrl.replace("https", "http")
+  }
+
 }
+
+// get stories linked to for loop
+// get url
+// see if you can get the image onto this obj
--- testingFramework.js
+++ testingFramework.js
@@ -17,11 +17,34 @@ function it(label, callback ) {
   callback()
 }
 
+console.log("Articles tests")
 
-it('can store article meta details ', function() {
+it('can store a story in an article grouping ', function() {
   var articles = new Articles
   var story = new Story("test story")
   articles.addArticle(story)
   expect(articles.viewArticles()[0].title).toEqual('test story');
 })
+
+console.log("Story tests")
+
+it('can add further meta details', function() {
+  var apiUrl = "https://content.guardianapis.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity"
+  var webUrl = "https://www.theguardian.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity"
+  var story = new Story("test story")
+  story.apiUrl = apiUrl
+  story.webUrl = webUrl
+  expect(story.apiUrl).toEqual("https://content.guardianapis.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity");
+  expect(story.webUrl).toEqual("https://www.theguardian.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity");
+})
+
+it('can update an api url ready fetch an article', function() {
+  var apiUrl = "https://content.guardianapis.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity"
+  var story = new Story("test story")
+  story.apiUrl = apiUrl
+  story.cleanUrl() 
+  expect(story.cleanApiUrl).toEqual("http://content.guardianapis.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity");
+})
+
+// id: "media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity"
 
